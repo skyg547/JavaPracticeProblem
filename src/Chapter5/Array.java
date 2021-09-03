b60f04199b5592769ce99fbe5219f6354e858800
skyg547 <skyg547@gmail.com>
2021-09-03 22:22:07 +0900

    inteli j test

---
+++ src/Chapter5/Array.java
@@ -0,0 +1,16 @@
+public class Exersise07 {
+
+    public static void main(String[] args) {
+
+        int max = 0;
+        int[] array = {1, 5, 3, 8, 2};
+
+
+
+        System.out.println("max : " + max);
+
+
+    }
+
+
+}
