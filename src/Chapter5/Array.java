c0cfd384ddca1ef702bfda2fe7db0b3c545c23c3
SungHo Ha <skyg547@gmail.com>
2021-09-04 20:03:56 +0900

    Class name chaged

--- src/Chapter5/Array.java
+++ src/Chapter5/Array.java
@@ -1,9 +1,12 @@
-public class Exersise07 {
+package Chapter5;
+
+public class Array{
 
     public static void main(String[] args) {
 
         int max = 0;
         int[] array = {1, 5, 3, 8, 2};
+
 
 
 
