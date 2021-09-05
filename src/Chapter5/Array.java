e67f89afb745505dfcf8ad24d40586c9c3b5c695
SungHo Ha <skyg547@gmail.com>
2021-09-05 15:03:40 +0900

    find out Max Value 2020905

--- src/Chapter5/Array.java
+++ src/Chapter5/Array.java
@@ -1,14 +1,19 @@
 package Chapter5;
 
-public class Array{
+public class Array {
 
     public static void main(String[] args) {
 
         int max = 0;
         int[] array = {1, 5, 3, 8, 2};
 
+        for ( int index : array
+             ) {
+            if( index > max ){
+                max = index;
+            }
 
-
+        }
 
         System.out.println("max : " + max);
 
