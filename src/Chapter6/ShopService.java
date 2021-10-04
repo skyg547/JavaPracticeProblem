482f95cf5222073b737994836326b2baa710465d
SungHo Ha <skyg547@gmail.com>
2021-10-04 15:37:46 +0900

    singleton pattern on service

---
+++ src/Chapter6/ShopService.java
@@ -0,0 +1,17 @@
+package Chapter6;
+
+public class ShopService {
+
+    private static ShopService shopService = new ShopService();
+
+    private ShopService(){
+
+
+    }
+
+
+    public static ShopService getInstance(){
+
+        return shopService;
+    }
+}
