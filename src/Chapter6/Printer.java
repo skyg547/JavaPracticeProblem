935b87b9a5da1c3602eed7d10f6e3fc5c02de4a9
SungHo Ha <skyg547@gmail.com>
2021-10-06 22:45:02 +0900

    Printer

---
+++ src/Chapter6/Printer.java
@@ -0,0 +1,30 @@
+package Chapter6;
+
+public class Printer {
+
+    public static void printer(int returnValue) {
+
+        System.out.println(returnValue);
+
+    }
+
+
+    public static void printer(boolean returnValue) {
+
+        System.out.println(returnValue);
+
+    }
+
+    public static void printer(double returnValue) {
+
+        System.out.println(returnValue);
+
+    }
+
+    public static void printer(String returnValue) {
+
+        System.out.println(returnValue);
+
+    }
+
+}
---
+++ src/Chapter6/PrinterExample.java
@@ -0,0 +1,14 @@
+package Chapter6;
+
+public class PrinterExample {
+
+    public static void main(String[] args){
+
+        Printer.printer(10);
+        Printer.printer(true);
+        Printer.printer(5.7);
+        Printer.printer("홍길동");
+
+    }
+
+}
