bca12e14bc3237f72edad810a87bf99b03dd8f33
skyg547 <skyg547@gmail.com>
2021-10-23 22:59:20 +0900

    chapter11 problem3 write Student class

---
+++ src/BasicAPIClass/Student.java
@@ -0,0 +1,18 @@
+package BasicAPIClass;
+
+public class Student {
+
+    private String studentNum;
+
+    public Student(String studentNUm){
+        this.studentNum = studentNum;
+
+
+    }
+
+    public String getSudentNum(){
+        return studentNum;
+
+
+    }
+}
