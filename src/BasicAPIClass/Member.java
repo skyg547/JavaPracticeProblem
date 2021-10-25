4a7650e6416811e895a4cbeb89493a03cfda0272
skyg547 <skyg547@gmail.com>
2021-10-25 21:12:49 +0900

    chapter11 problem4 write Member.java

---
+++ src/BasicAPIClass/Member.java
@@ -0,0 +1,14 @@
+package BasicAPIClass;
+
+public class Member {
+    private String id;
+    private String name;
+
+    public Member(String id, String name){
+        this.id = id;
+        this.name = name;
+
+
+    }
+
+}
