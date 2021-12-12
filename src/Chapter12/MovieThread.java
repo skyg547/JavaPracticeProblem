ba3bf0ab4adb4f7fa0bc7820eaf09bda50c22763
SungHo Ha <skyg547@gmail.com>
2021-12-12 21:29:06 +0900

    problem 10

--- src/Chapter12/MovieThread.java
+++ src/Chapter12/MovieThread.java
@@ -1,5 +1,7 @@
 package Chapter12;
 
+import java.util.function.DoubleToIntFunction;
+
 public class MovieThread extends Thread {
 
     @Override
@@ -14,7 +16,22 @@ public class MovieThread extends Thread {
             }
 
         }
+
     }
+
+    // 10
+//    @Override
+//    public void run(){
+//
+//        while (true){
+//
+//            System.out.println("동영상을 재생합니다");
+//
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {}
+//        }
+//    }
 
 
 }
