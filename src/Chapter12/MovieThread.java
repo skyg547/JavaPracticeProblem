8df61fceebf0aba3a7f37282537072056e034ef0
skyg547 <skyg547@gmail.com>
2021-12-18 23:37:40 +0900

    problem  8

--- src/Chapter12/MovieThread.java
+++ src/Chapter12/MovieThread.java
@@ -3,19 +3,51 @@ package Chapter12;
 import java.util.function.DoubleToIntFunction;
 
 public class MovieThread extends Thread {
+    /**
+     * 1번
+     *
+     */
+//    @Override
+//    public void run() {
+//
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println("동영상을 재생합니다.");
+//
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//            }
+//
+//        }
+//
+//    }
+
+    /**
+     * 8번
+     */
 
     @Override
-    public void run() {
+    public void run(){
+        while (true){
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("동영상을 재생합니다.");
+            System.out.println("동영상을 재생합니다");
 
+            // 방법 1
             try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
+                Thread.sleep(1);
+            } catch (Exception e){
+
+                break;
+            }
+            // 방법 2
+            // this.isInterrupted() 같다
+            if(Thread.interrupted()){
+                break;
+
             }
 
         }
+
 
     }
 
