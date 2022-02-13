6058cd2ec71f3b2c305d8b5b68b43ca8301fdfd2
skyg547 <skyg547@gmail.com>
2022-02-13 21:05:49 +0900

    GENRIG BOXING

--- src/Chapter13/Box.java
+++ src/Chapter13/Box.java
@@ -1,16 +1,42 @@
 package Chapter13;
 
-public class Box {
 
-    private Object object;
+// 제네릭 이전 박스 클래스
+//public class Box {
+//
+//    private Object object;
+//
+//    public void setObject(Object object) {
+//        this.object = object;
+//    }
+//
+//    public Object get() {
+//        return object;
+//    }
+//
+//
+//}
 
-    public void setObject(Object object) {
-        this.object = object;
+public class Box<T> {
+
+    private T t ;
+
+    public void setT(T t) {
+        this.t = t;
     }
 
-    public Object get() {
-        return object;
+    public T getT() {
+        return t;
     }
-
-
 }
+
+
+/* 다음과 같이 재구성 된다
+*  public class Box<String> {
+*       private String t;
+*       public void set(String t ) { this.t  = t ;}
+*       public String get() {return t; }
+* }
+*
+*
+* */
--- src/Chapter13/BoxExample.java
+++ src/Chapter13/BoxExample.java
@@ -2,14 +2,28 @@ package Chapter13;
 
 public class BoxExample {
 
+    // 제네릭 변경 전
+//    public static void main(String[] args) {
+//        Box box = new Box();
+//        box.setObject("홍길동");
+//
+//        String name = (String) box.get(); // 강제 타입 변환
+//
+//
+//        box.setObject(new Apple()); // 자동 타입 변환
+//        Apple apple = (Apple)  box.get(); // 강제 타입 변환
+//    }
+
+    /**
+    * @package : Chapter13
+    * @name : BoxExample.java
+    * @date : 2022-02-13 오후 4:26
+    * @author : H.sung.ho
+    * @version : 1.0.0
+    * @modifyed : 제네릭 변경 후
+    **/
     public static void main(String[] args) {
-        Box box = new Box();
-        box.setObject("홍길동");
-
-        String name = (String) box.get(); // 강제 타입 변환
 
 
-        box.setObject(new Apple()); // 자동 타입 변환
-        Apple apple = (Apple)  box.get(); // 강제 타입 변환
     }
 }
