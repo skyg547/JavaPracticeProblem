4cf06aecf2a18ade8ab13a272b5c720991434bb7
skyg547 <skyg547@gmail.com>
2022-04-13 23:40:49 +0900

    FunctionExample1

--- src/BasicAPIClass/Student.java
+++ src/BasicAPIClass/Student.java
@@ -3,15 +3,51 @@ package BasicAPIClass;
 public class Student {
 
     private String studentNum;
+    private String studentName;
+    private Integer studentAge;
+    private String studentSex;
 
-    public Student(String studentNUm) {
-        this.studentNum = studentNUm;
+    public Student() {
     }
 
-    public String getSudentNum() {
+    public Student(String studentNum, String studentName, Integer studentAge, String studentSex) {
+        this.studentNum = studentNum;
+        this.studentName = studentName;
+        this.studentAge = studentAge;
+        this.studentSex = studentSex;
+    }
+
+    public Integer getStudentAge() {
+        return studentAge;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getStudentNum() {
         return studentNum;
     }
 
+    public String getStudentSex() {
+        return studentSex;
+    }
+
+    public void setStudentAge(Integer studentAge) {
+        this.studentAge = studentAge;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setStudentNum(String studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public void setStudentSex(String studentSex) {
+        this.studentSex = studentSex;
+    }
     @Override
     public int hashCode() {
         return Integer.parseInt(studentNum);
@@ -28,7 +64,7 @@ public class Student {
     public boolean equals(Object obj) {
         if (obj instanceof Student) {
             Student student = (Student) obj;
-        if(studentNum.equals(student.getSudentNum()))
+        if(studentNum.equals(student.getStudentNum()))
             return true;
         }
         return false;
--- src/Chapter14/FunctionExample1.java
+++ src/Chapter14/FunctionExample1.java
@@ -5,21 +5,35 @@ import BasicAPIClass.Student;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.ToIntFunction;
 
 public class FunctionExample1 {
 
-    private static List<Student> list = Arrays.asList(
-            new Student("John"),
-            new Student("Mary"),
-            new Student("Peter"),
-            new Student("Paul")
+    private final static List<Student> list = Arrays.asList(
+            new Student( "2","John", 18, "F" ),
+            new Student("3","Mary" , 13, "M"),
+            new Student("4","Peter", 34, "M"),
+            new Student("5", "Paul", 22, "M"),
+            new Student("6", "Mike", 23, "M")
+
+
     );
 
     public static void printString(Function<Student, String> f) {
         list.forEach(s -> System.out.println(f.apply(s)));
     }
 
+    public static void printInt(ToIntFunction<Student> f) {
+        list.forEach(s -> System.out.println(f.applyAsInt(s)));
+
+
+    }
+
+
+
     public static void main(String[] args) {
-        printString(Student::getSudentNum);
+        printString(Student::getStudentNum);
+
+        printInt(Student::getStudentAge);
     }
 }
