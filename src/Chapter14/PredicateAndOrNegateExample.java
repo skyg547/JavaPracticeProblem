b57877700d000d00c3eb7138c05d4910ceec10fa
skyg547 <skyg547@gmail.com>
2022-04-23 23:41:41 +0900

    PredicateAndOrNegateExample

--- src/Chapter14/PredicateAndOrNegateExample.java
+++ src/Chapter14/PredicateAndOrNegateExample.java
@@ -5,11 +5,11 @@ import java.util.function.IntPredicate;
 public class PredicateAndOrNegateExample {
 
     public static void main(String[] args) {
-        IntPredicate predicateA = a -> a%2 == 0;
+        IntPredicate predicateA = a -> a % 2 == 0;
 
-        IntPredicate predicateB = a -> a%3 == 0;
+        IntPredicate predicateB = a -> a % 3 == 0;
 
-        IntPredicate predicateC = a -> a%5 == 0;
+        IntPredicate predicateC = a -> a % 5 == 0;
 
         IntPredicate predicateABC;
         boolean result;
@@ -20,5 +20,18 @@ public class PredicateAndOrNegateExample {
 
         result = predicateABC.test(30);
         System.out.println("15 is divisible by 2, 3 and 5: " + result);
+
+        //or()
+        predicateABC = predicateA.or(predicateB).or(predicateC);
+        result = predicateABC.test(5);
+        System.out.println(
+                "5 is divisible by 2, 3 or 5: " + result);
+
+        //negate()
+        predicateABC = predicateA.negate();
+        result = predicateABC.test(5);
+        System.out.println("5 is divisible by 2: " + result);
+
+
     }
 }
