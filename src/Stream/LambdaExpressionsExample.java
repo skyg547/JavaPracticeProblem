3ff75cf801cbe3a44353fbf44b0360728aad9927
SungHo Ha <44772145+skyg547@users.noreply.github.com>
2022-05-28 10:18:52 +0000

    Strea

--- src/Stream/LambdaExpressionsExample.java
+++ src/Stream/LambdaExpressionsExample.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class LambdaExpressionsExample {
     public static void main(String[] args) {
-        List<Student> list = Arrays.asList(new Student("g",54));
-        list.stream().filter(s -> s.getScore() > 50).forEach(s -> System.out.println(s.getName()));
+        List<Student> list = Arrays.asList(new Student("g",54), new Student("gs",554), new Student("gh",58));
+        list.stream().filter(s -> s.getScore() > 50).forEach(s -> System.out.println(s.getName()+ " " + s.getScore()));
     }
 }
