ca061e71a2437f46834923fd437b1672bd004c61
SungHo Ha <44772145+skyg547@users.noreply.github.com>
2022-05-28 10:30:59 +0000

    ParalleExample

--- src/Stream/ParalleExample.java
+++ src/Stream/ParalleExample.java
@@ -7,18 +7,17 @@ import java.util.stream.Stream;
 public class ParalleExample {
 
     public static void main(String[] args) {
-        // TODO code application logic here
         List<String> list = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 
         Stream<String> stream = list.stream();
-        Stream<String> stream2 = list.stream();
+        Stream<String> stream2 = list.parallelStream();
 
-        stream.forEach(System.out::println);
+        stream.forEach(ParalleExample::print);
         stream2.forEach(ParalleExample::print);
 
     }
 
     public static void print(String s){
-        System.out.println(s);
+        System.out.println(s+ ":" + Thread.currentThread().getName());
     }
 }
