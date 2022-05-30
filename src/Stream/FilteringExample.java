52939d53ce94da53a748afdfad87d0ebbfeca98f
SungHo Ha <44772145+skyg547@users.noreply.github.com>
2022-05-30 13:45:13 +0000

    [Fix] Filter and distinct

--- src/Stream/FilteringExample.java
+++ src/Stream/FilteringExample.java
@@ -1,9 +1,22 @@
 package Stream;
 
-import com.ho.Main;
+import java.util.Arrays;
+import java.util.List;
 
 public class FilteringExample {
-    
 
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("adam", "adam", "adam", "anna", "jack", "jenny");
+
+        names.stream().distinct().forEach(n -> System.out.println(n));
+
+        System.out.println("--------------------------------");
+
+        names.stream().filter(n -> n.startsWith("a")).forEach(n -> System.out.println(n));
+
+        System.out.println("--------------------------------");
+        names.stream().distinct().filter(predicate -> predicate.startsWith("a")).forEach(n -> System.out.println(n));
+
+    }
 
 }
