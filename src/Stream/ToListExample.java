5e4d3456743beea6eb637a19f5b156544a985f74
SungHo Ha <44772145+skyg547@users.noreply.github.com>
2022-06-07 14:49:16 +0000

    ToListClass

---
+++ src/Stream/ToListExample.java
@@ -0,0 +1,13 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.ho.Main;
+
+public class ToListExample {
+    public static void main(){
+
+        List<Student> totalList = Arrays.asList(new Student())
+    }
+}
